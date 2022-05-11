3e0fc3f83c8163b7ac8bf4b9f2766d1fc3bc81ae
pavelkrivokorytov <paxa-net@mail.ru>
2022-05-11 11:02:27 +0300

    Initial commit

---
+++ src/main/java/com/spring/springdz/SpringdzApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springdz/SpringdzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
